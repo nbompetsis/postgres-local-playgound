7ad53df071a87d250d5cd62447183a5cf2cbc77e
Nikolas Bompetsis <nbompetsis@di.uoa.gr>
2023-07-07 19:17:02 +0300

    Adding service-user to superusers

--- files/init.sql
+++ files/init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE "service_db";
-CREATE USER "service-user" WITH ENCRYPTED PASSWORD 'password';
+CREATE USER "service-user" WITH SUPERUSER ENCRYPTED PASSWORD 'password';
 
 GRANT ALL PRIVILEGES ON DATABASE "service_db" TO "service-user";
